4e9b855761f7f4903fff05b13ab3c15941d21007
arissketch <60331491+arissketch@users.noreply.github.com>
2024-05-23 03:39:57 +0000

    comment

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -19,16 +19,17 @@ class RatingsController < ApplicationController
 
   def create
     the_rating = Rating.new
-    the_rating.comment = params.fetch("query_comment")
+    the_rating.comment = params.fetch("query_comments")
     the_rating.user_id = current_user.id
     the_rating.cleaniness = params.fetch("query_cleaniness")
     the_rating.safety = params.fetch("query_safety")
     the_rating.bathroom_id = params.fetch("bathroom_id")
     the_rating.environment = params.fetch("query_environment")
+    the_rating.overall=(the_rating.cleaniness+the_rating.environment+the_rating.safety)/3
 
     if the_rating.valid?
       the_rating.save
-      redirect_to("/bathrooms/bathroom_id", { :notice => "Rating created successfully." })
+      redirect_to("/bathrooms/#{the_rating.bathroom_id}", { :notice => "Rating created successfully." })
     else
       redirect_to("/ratings", { :alert => the_rating.errors.full_messages.to_sentence })
     end
